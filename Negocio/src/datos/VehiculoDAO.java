f03f70a7bcef4676a4c02ebbab8ede57d2658637
Dalonxon <porrassergio0990@gmail.com>
2024-05-26 21:50:56 -0500

    "lo k esta bien"

--- Negocio/src/datos/VehiculoDAO.java
+++ Negocio/src/datos/VehiculoDAO.java
@@ -31,81 +31,13 @@ public class VehiculoDAO {
      * Incluye una nueva fila en la tabla EMPLOYEES.
      * @throws RHException
      */
-    public  void incluirVehiculo(Vehiculo Vehiculo, Plaza Plaza) throws PYException {
-      try {
-      
-        String strSQL = "INSERT INTO Vehiculo (k_placa, i_tipo, k_nomenclatura) VALUES(?,?,?)";
-        Connection conexion = ServiceLocator.getInstance().tomarConexion();
-        PreparedStatement prepStmt = conexion.prepareStatement(strSQL);
-        prepStmt.setString(1, Vehiculo.getPlaca()); 
-        prepStmt.setString(2, Vehiculo.getTipo()); 
-        prepStmt.setString(3, Plaza.getidPlaza()); 
-        prepStmt.executeUpdate();
-        prepStmt.close();
-        ServiceLocator.getInstance().commit();
-        
-        
-      } catch (SQLException e) {
-           ServiceLocator.getInstance().rollback();
-           throw new PYException( "EmpleadoDAO", "No pudo crear el vehiculo"+ e.getMessage());
-      }  finally {
-         ServiceLocator.getInstance().liberarConexion();
-      }
-      
-    }
     
-    public Plaza buscarPlaza(String nomenclaturaPlaza) throws PYException {
-        boolean existe = false;
-        try {
-            Plaza e = new Plaza(); //Instancia el objeto para retornar los datos del empleado
-            String strSQL = "SELECT k_nomenclatura, i_estaocupado FROM plaza WHERE k_nomenclatura = ?";
-            Connection conexion = ServiceLocator.getInstance().tomarConexion();
-            PreparedStatement prepStmt = conexion.prepareStatement(strSQL);
-            prepStmt.setString(1, nomenclaturaPlaza);
-            ResultSet rs = prepStmt.executeQuery();
-            while (rs.next()) {
-                e.setidPlaza(rs.getString("k_nomenclatura"));
-                e.setEstado(rs.getBoolean("i_estaocupado"));
-                existe = true;
-            }
-            if (existe) 
-                return e;
-             else
-                return null;
-        } catch (SQLException e) {
-            throw new PYException("VahiculoDAO", e.getMessage());
-        } finally {
-            ServiceLocator.getInstance().liberarConexion();
-        }
-    }
     
-    public void updatePlaza(Plaza plaza, Vehiculo vehiculo) throws PYException{
-        try {
-        String strSQL = "UPDATE plaza set i_estaocupado = ?, k_placa = ? where k_nomenclatura = ?";
-        Connection conexion = ServiceLocator.getInstance().tomarConexion();
-        PreparedStatement prepStmt = conexion.prepareStatement(strSQL);
-        prepStmt.setBoolean(1, plaza.estaOcupado()); 
-        prepStmt.setString(2, vehiculo.getPlaca()); 
-        prepStmt.setString(3, plaza.getidPlaza()); 
-        prepStmt.executeUpdate();
-        prepStmt.close();
-        ServiceLocator.getInstance().commit();
-        
-        
-      } catch (SQLException e) {
-           ServiceLocator.getInstance().rollback();
-           throw new PYException( "EmpleadoDAO", "No pudo crear el empleado"+ e.getMessage());
-      }  finally {
-         ServiceLocator.getInstance().liberarConexion();
-      }
-      
-    }
-    
-<<<<<<< Pallo
 
-    public void eliminarEmpleado(){
+
+    
       //implementar
-=======
+
      public void registrarSalidaVehiculo(String placa, LocalDateTime fechaSalida) {
         Connection conexion = null;
         CallableStatement stmt = null;
@@ -143,7 +75,7 @@ public class VehiculoDAO {
                 se.printStackTrace();
             }
         }
->>>>>>> main
+
     }
      
     public void insertarFechaHoraEnBD(OffsetDateTime fechaHora) throws PYException {
